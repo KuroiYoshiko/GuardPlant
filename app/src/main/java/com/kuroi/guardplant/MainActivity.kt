b4ed6599fdaa45d04a6b2a9fc3d0e7b783b87300
Milena Karolewska <milenakarolewska@wp.pl>
2023-08-26 18:23:51 +0200

    added view binding

--- app/src/main/java/com/kuroi/guardplant/MainActivity.kt
+++ app/src/main/java/com/kuroi/guardplant/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kuroi.guardplant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
