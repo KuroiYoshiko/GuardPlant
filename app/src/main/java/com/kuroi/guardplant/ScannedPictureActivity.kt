b5571884b005910972f7a7f85c91e668e4dfd7f7
Milena Karolewska <milenakarolewska@wp.pl>
2023-08-26 23:00:16 +0200

    bitmap to base64 converter

--- app/src/main/java/com/kuroi/guardplant/ScannedPictureActivity.kt
+++ app/src/main/java/com/kuroi/guardplant/ScannedPictureActivity.kt
@@ -2,6 +2,7 @@ package com.kuroi.guardplant
 
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Base64
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,6 +10,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.kuroi.guardplant.databinding.ActivityScannedPictureBinding
+import java.io.ByteArrayOutputStream
 
 class ScannedPictureActivity : AppCompatActivity() {
 
@@ -22,11 +24,24 @@ class ScannedPictureActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val receivedIntent = intent
+
         if (receivedIntent != null && receivedIntent.hasExtra("plantImage")) {
-            val plantImage: Bitmap? = receivedIntent.getParcelableExtra("plantImage")
+            val plantImage: Bitmap? = receivedIntent.getParcelableExtra("plantImage")!!
+            val plantBase64Img = bitmapToBase64(plantImage!!)
+
             binding.ivScannedPicture.setImageBitmap(plantImage)
+            
+
         }
 
+
+    }
+
+    private fun bitmapToBase64(bitmap: Bitmap): String {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+        val byteArray = byteArrayOutputStream.toByteArray()
+        return Base64.encodeToString(byteArray, Base64.DEFAULT)
     }
 
 }
