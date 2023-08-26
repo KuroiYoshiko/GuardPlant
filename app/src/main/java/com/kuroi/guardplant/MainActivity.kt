4d041d790ce5183a00782e6b6799e9424cea99a3
Milena Karolewska <milenakarolewska@wp.pl>
2023-08-26 15:30:28 +0200

    create new project

---
+++ app/src/main/java/com/kuroi/guardplant/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kuroi.guardplant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
