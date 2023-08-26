4ea3efe313d2c28bdd7bab84110c3df6fa5bb570
Milena Karolewska <milenakarolewska@wp.pl>
2023-08-26 19:33:07 +0200

    added navigation between fragments

--- app/src/main/java/com/kuroi/guardplant/MainActivity.kt
+++ app/src/main/java/com/kuroi/guardplant/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.kuroi.guardplant
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.kuroi.guardplant.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.bottomMenu_home -> replaceFragment(HomeFragment())
+                R.id.bottomMenu_diagnose -> replaceFragment(DiagnoseFragment())
+                R.id.bottomMenu_reminders -> replaceFragment(RemindersFragment())
+                R.id.bottomMenu_myPlants -> replaceFragment(MyPlantsFragment())
+                else -> replaceFragment(HomeFragment())
+            }
+            true
+        }
+        
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.mainFrameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
