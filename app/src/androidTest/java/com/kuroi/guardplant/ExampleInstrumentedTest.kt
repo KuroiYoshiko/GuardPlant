cb1d735798ac0ae2695d4db75ea76720933139c3
KuroiYoshiko <milena.karolewska@gmail.com>
2026-07-15 13:37:23 +0200

    chore: init

---
+++ app/src/androidTest/java/com/kuroi/guardplant/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.kuroi.guardplant
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.kuroi.guardplant", appContext.packageName)
+    }
+}
